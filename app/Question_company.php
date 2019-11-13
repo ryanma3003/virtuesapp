ea417acb488ce536c08536f395c0bdea544d6050
ryanma3003 <ryanmartin3003@gmail.com>
2019-11-13 18:50:59 +0700

    first commit

---
+++ app/Question_company.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question_company extends Model
+{
+    protected $table = 'question_company';
+    public $timestamps = true;
+    protected $fillable = array('kd_virtues_id', 'tanya1', 'tanya2', 'tanya3', 'tanya4', 'tanya5', 'tanya6', 'tanya7', 'tanya8', 'tanya9', 'tanya10', 'tanya11', 'tanya12');
+
+    public function kd_virtues()
+    {
+        return $this->belongsTo('App\Kd_virtues');
+    }
+}
